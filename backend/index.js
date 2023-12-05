9ae38bf4059126582ffb1267d0f36a401390ede4
taufiqulf <taufiqulf2@gmail.com>
2023-12-05 21:46:11 +0700

    Update on Package.json for audit compatibility, move react-script to devdependencies

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,90 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const routes = require('./routes/router.js'); // Import routes
+#!/usr/bin/env node
 
-const app = express();
+/**
+ * Module dependencies.
+ */
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var app = require('../app');
+var debug = require('debug')('bangkit-ielts-cloud:server');
+var http = require('http');
 
-const corsOptions = {
-    origin: '*', // This allows all origins, consider restricting this in production
-    credentials: true,
-    optionSuccessStatus: 200
-};
-app.use(cors(corsOptions));
-app.use('/', routes); // Use the imported routes
+/**
+ * Get port from environment and store in Express.
+ */
 
-const port = 5050;
-const server = app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+var port = normalizePort(process.env.PORT || '5050');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
