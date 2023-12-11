4a5e076869c44bda654482c2382c636619a2cc4c
MHALtd <hasbi1117@gmail.com>
2023-12-12 01:32:01 +0700

    add route button

--- src/pages/components/Test.jsx
+++ src/pages/components/Test.jsx
@@ -8,7 +8,7 @@ const Test = () => {
   return (
     <div className="md:px-14 p-4 max-w-s mx-auto space-y pb-20" id="test">
       <div className="text-center">
-        <h2 className="text-5xl font-extrabold text-secondary mb-2">Take Your Test</h2>
+        <h2 className="text-3xl sm:text-5xl text-secondary font-bold mb-2">Take Your Test</h2>
       </div>
       <p className="text-secondary max-w-s mx-auto px-4 text-xl text-center">
         Master the language of success with English Buddy for IELTS Excellence! #IELTSReady #EnglishBuddy
@@ -16,37 +16,34 @@ const Test = () => {
 
       {/* TEST CARD */}
       <div className="mt-16">
-        <div>
-          <a href="/">
-            <div className="flex items-center justify-center pb-12">
-              <button className="btnPrimary text-3xl text-brokenwhite">Take Your Test</button>
-            </div>
-          </a>
-        </div>
         <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-10 max-w-screen mx-auto ">
           <Card
             ilust={listening}
             title={"Listening Test"}
             desc={"Sharpen your listening skills with our practice exercises and simulated test environments."}
-            btn2={"Start Test"}
+            path={"src/pages/listening.html"}
+            btn2={"Take Listening Test"}
           />
           <Card
             ilust={reading}
             title={"Reading Test"}
             desc={"Improve your reading comprehension with a variety of practice passages and quizzes."}
-            btn2={"Start Test"}
+            path={"src/pages/reading.html"}
+            btn2={"Take Reading Test"}
           />
           <Card
             ilust={writing}
             title={"Writing Test"}
             desc={"Enhance your writing skills through guided quizzes and essay writing practices."}
-            btn2={"Start Test"}
+            path={"src/pages/writing.html"}
+            btn2={"Take Writing Test"}
           />
           <Card
             ilust={speaking}
             title={"Speaking Test"}
             desc={"Practice your spoken English skills with our interactive speaking test simulations."}
-            btn2={"Start Test"}
+            path={"src/pages/speaking.html"}
+            btn2={"Take Speaking Test"}
           />
         </div>
       </div>
