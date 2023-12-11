90690a6794725b18a60c8777c67f97911c198c6e
MHALtd <hasbi1117@gmail.com>
2023-12-12 01:30:08 +0700

    move route test button

--- src/pages/shared/Card.jsx
+++ src/pages/shared/Card.jsx
@@ -1,12 +1,17 @@
-const Card = ({ ilust, title, desc }) => {
+const Card = ({ ilust, title, desc, btn2, path }) => {
   return (
     <div>
-      <div className="border py-10 md:px-6 px-4 rounded-[35px] shadow-3xl hover:-translate-y-4 transition-all duration-300">
+      <div className="border py-10 md:px-6 px-4 rounded-[35px] shadow-3xl">
         <h3 className="text-3xl font-bold text-center text-secondary">{title}</h3>
         <div className="flex items-center justify-center">
           <img src={ilust} alt="" />
         </div>
         <p className="text-secondary text-center my-5">{desc}</p>
+        <div className="flex items-center justify-center">
+          <a href={path}>
+            <button className="btnPrimary text-2xl text-brokenwhite">{btn2}</button>
+          </a>
+        </div>
       </div>
     </div>
   );
