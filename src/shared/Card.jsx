5fa50296e825f9d3a5fb76416100f3eef8baaf26
Muhammad Hasbi Abdillah <119222428+MHALtd@users.noreply.github.com>
2023-12-11 17:01:49 +0700

    Update Card.jsx

--- src/shared/Card.jsx
+++ src/shared/Card.jsx
@@ -1,15 +1,12 @@
-const Card = ({ ilust, title, desc, btn2 }) => {
+const Card = ({ ilust, title, desc }) => {
   return (
     <div>
-      <div className="border py-10 md:px-6 px-4 rounded-lg shadow-3xl">
+      <div className="border py-10 md:px-6 px-4 rounded-[35px] shadow-3xl hover:-translate-y-4 transition-all duration-300">
         <h3 className="text-3xl font-bold text-center text-secondary">{title}</h3>
         <div className="flex items-center justify-center">
           <img src={ilust} alt="" />
         </div>
         <p className="text-secondary text-center my-5">{desc}</p>
-        <div className="space-x-5 space-y-4 pl-3">
-          <button className="btnPrimary text-xl">{btn2}</button>
-        </div>
       </div>
     </div>
   );
